f05034b86555232a97ede55e6541ebe75d17038f
maitreyeep2 <maitreyeeparkhi2@gmail.com>
2021-07-13 10:58:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+  cors: {
+  origin: '*'}
+})
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId,userName) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId,userName)
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message,userName)
+    
+    
+    })
+        
+  })
+})
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId,userName) => {
+    socket.join(roomId)
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    
+  
+    })
+  })
+})
+server.listen(process.env.PORT || 3000)
